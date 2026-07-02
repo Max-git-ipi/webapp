b41bd85f7a415dc998ca4fd9d95340bf32f1fc9c
Max-git-ipi <maximilien.paul@edu.igensia.com>
2026-07-02 13:39:22 +0200

    Add test for calculating sums with branching

--- app.test.js
+++ app.test.js
@@ -1,4 +1,9 @@
 const calcul = require('./app');
+
 test('adds 1 + 2 to equal 3', () => {
   expect(calcul(1, 2)).toBe(3);
+});
+
+test('adds 15 + 5 to equal 20 (branche if a > 10)', () => {
+  expect(calcul(15, 5)).toBe(20);
 });
